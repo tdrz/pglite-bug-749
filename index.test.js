82dc826511cbd15fbd73b30df9a072ffac50de77
Daniel Bartholomae <daniel@bartholomae.name>
2025-07-30 12:14:40 +0200

    Fix test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,20 @@
-import { PGlite} from "@electric-sql/pglite";
-import { it, expect } from "vitest";
+import {PGlite} from "@electric-sql/pglite";
+import {it, expect} from "vitest";
 
-it("fails", async() => {
-    const db = new PGlite({ debug: 5 })
-    expect(await db.query("select 'Hello world' as message;")).toEqual({ message: "Hello world" });
+it("fails", async () => {
+    const db = new PGlite({debug: 5})
+    expect(await db.query("select 'Hello world' as message;")).toEqual({
+        "affectedRows": 0,
+        "fields": [
+            {
+                "dataTypeID": 25,
+                "name": "message",
+            },
+        ],
+        "rows": [
+            {
+                "message": "Hello world",
+            },
+        ],
+    });
 })
