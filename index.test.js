d8b4e7988c75aea20e65b7491d5dc00556c57b60
Daniel Bartholomae <daniel@bartholomae.name>
2025-07-30 12:12:42 +0200

    Add more debugging logs

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@ import { PGlite} from "@electric-sql/pglite";
 import { it, expect } from "vitest";
 
 it("fails", async() => {
-    const db = new PGlite()
+    const db = new PGlite({ debug: 5 })
     expect(await db.query("select 'Hello world' as message;")).toEqual({ message: "Hello world" });
 })
